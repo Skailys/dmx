51fac7fb937725d4c3b332fa59e770ef1eaf97f0
fredie04 <me@fredie04.dev>
2024-06-16 12:06:24 +0200

    Added relative control class for DMX_Serial and movement tests

---
+++ dmx/relative.py
@@ -0,0 +1,80 @@
+from dmx import DMX_Serial
+
+
+class RelativeControl(DMX_Serial):
+    """
+    Represents a relative control for DMX_Serial.
+
+    Attributes:
+        x (int): The x v alue.
+        xtrim (int): The x trim value.
+        y (int): The y value.
+        ytrim (int): The y trim value.
+        xy_speed (int): The xy speed value.
+    """
+
+    def __init__(self, offset=0, port="/dev/ttyUSB0"):
+        self.port = port
+        self.offset = 0
+
+        self.x = 0
+        self.xtrim = 0
+        self.y = 0
+        self.ytrim = 0
+        self.xy_speed = 0
+
+        super().__init__()
+
+    def add_x(self, value):
+        self.x = overflow_add(self.x, value, 256)
+
+    def add_y(self, value):
+        self.y = overflow_add(self.y, value, 256)
+
+    def add_xtrim(self, value):
+        self.xtrim = overflow_add(self.xtrim, value, 256)
+
+    def add_ytrim(self, value):
+        self.ytrim = overflow_add(self.ytrim, value, 256)
+
+    def add_xy_speed(self, value):
+        self.xy_speed = overflow_add(self.xy_speed, value, 256)
+
+    def set_x(self, value):
+        assert 0 <= value <= 255
+        self.x = value
+
+    def set_y(self, value):
+        assert 0 <= value <= 255
+        self.y = value
+
+    def set_xtrim(self, value):
+        assert 0 <= value <= 255
+        self.xtrim = value
+
+    def set_ytrim(self, value):
+        assert 0 <= value <= 255
+        self.ytrim = value
+
+    def set_xy_speed(self, value):
+        assert 0 <= value <= 255
+        self.xy_speed = value
+
+    def get_data(self):
+        offset = self.offset
+        assert 0 <= offset <= 512 - 13  # 13 is the number of channels used by beam lamp
+
+        data = [0] * 512
+        data[0 + offset] = self.x
+        data[1 + offset] = self.xtrim
+        data[2 + offset] = self.y
+        data[3 + offset] = self.ytrim
+        data[4 + offset] = self.xy_speed
+
+        return bytes(data)
+
+    def send(self):
+        self.set_data(self.get_data())
+
+def overflow_add(a, b, max_value):
+    return (a + b) % max_value
---
+++ tests/movement.py
@@ -0,0 +1,85 @@
+from time import sleep
+import relative
+
+
+def level1():
+    turret = relative.RelativeControl()
+
+    turret.set_x(128)
+    turret.send()
+    sleep(1)
+
+    turret.add_x(10)
+    turret.send()
+    sleep(1)
+
+    turret.add_x(-20)
+    turret.send()
+    sleep(1)
+
+    turret.add_x(10)
+    turret.send()
+    sleep(1)
+
+    # test: by now, the turret should be back to its original position
+
+
+def level2():
+    turret = relative.RelativeControl()
+
+    turret.set_y(128)
+    turret.send()
+    sleep(1)
+
+    turret.add_y(10)
+    turret.send()
+    sleep(1)
+
+    turret.add_y(-20)
+    turret.send()
+    sleep(1)
+
+    turret.add_y(10)
+    turret.send()
+    sleep(1)
+
+    # test: by now, the turret should be back to its original position
+
+
+def level3():
+    turret = relative.RelativeControl()
+
+    turret.set_x(128)
+    turret.set_y(128)
+    turret.send()
+    sleep(1)
+
+    turret.add_x(10)
+    turret.add_y(10)
+    turret.send()
+    sleep(1)
+
+    turret.add_x(-20)
+    turret.add_y(-20)
+    turret.send()
+    sleep(1)
+
+    turret.add_x(10)
+    turret.add_y(10)
+    turret.send()
+    sleep(1)
+
+    # test: by now, the turret should be back to its original position
+
+
+if __name__ == "__main__":
+    input("Press Enter to start level 1")
+    level1()
+
+    input("Press Enter to start level 2")
+    level2()
+
+    input("Press Enter to start level 3")
+    level3()
+
+    print("All tests passed!")
