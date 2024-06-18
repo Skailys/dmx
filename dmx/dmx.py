948e28690e15604eba061606f96939756a813ce3
fredie04 <me@fredie04.dev>
2024-06-18 11:51:36 +0200

    chore: Fix typo in DMX_Serial class

--- dmx/dmx.py
+++ dmx/dmx.py
@@ -40,7 +40,7 @@ class DMX_Serial:
                 # Linux does not have proper support for variable length breaks, as the behavior of TCSBRK is
                 # undefined for values other than 0. (http://man7.org/linux/man-pages/man2/ioctl_tty.2.html)
                 # Instead this controls the timing of the break directly.
-                fcntl.ioctl(self.ser.desc 0x5427) # Yeah, it's magic. Start Break (TIOCSBRK)
+                fcntl.ioctl(self.ser.desc, 0x5427) # Yeah, it's magic. Start Break (TIOCSBRK)
                 time.sleep(0.0001)
                 fcntl.ioctl(self.ser.desc, 0x5428) # Yeah, it's magic. End Break (TIOCCBRK)
             else:
